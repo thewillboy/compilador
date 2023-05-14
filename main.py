97d44442070f3bbc89fcb5593af169250d8f2d85
thewillboy <72840708+thewillboy@users.noreply.github.com>
2023-05-14 19:02:50 -0300

    Adicionando novos tokens

--- main.py
+++ main.py
@@ -60,8 +60,10 @@ for arquivo in arquivos:
 
 class BasicLexer(Lexer):
   tokens = {
-    NAME, INT, REAL, STRING, IF, THEN, ELSE, FOR, DIGITAL_READ, DIGITAL_WRITE,
-    WHILE, FUN, TO, ARROW, EQEQ
+    NAME, TYPE_INT, TYPE_REAL, TYPE_STRING, INT, REAL, STRING, IF, THEN, ELSE,
+    END, FOR, TO, DIGITAL_READ, DIGITAL_WRITE, ANALOG_READ, ANALOG_WRITE,
+    PIN_MODE, PIN_TYPE, DO, WHILE, EQEQ, BOOL, BIGGER_OR_EQUAL,
+    SMALLER_OR_EQUAL, BIGGER, SMALLER, DELAY, LBRACE, RBRACE
   }
   ignore = '\t\b '
   #http://www.java2s.com/Code/Python/String/EscapeCodesbtnar.htm
@@ -74,25 +76,41 @@ class BasicLexer(Lexer):
   FOR = r'FOR'
   DIGITAL_READ = r'DIGITAL_READ'
   DIGITAL_WRITE = r'DIGITAL_WRITE'
+  ANALOG_READ = r'ANALOG_READ'
+  ANALOG_WRITE = r'ANALOG_WRITE'
+  DELAY = r'DELAY'
+  PIN_MODE = r'PIN_MODE'
+  DO = r'DO'
   WHILE = r'WHILE'
-  #DIGITAL_WRITE.*?\(\d\)$
-  FUN = r'FUN'
+  END = r'END'
+  LBRACE = r'{'
+  RBRACE = r'}'
+  #FUN = r'FUN'
   TO = r'TO'
-  ARROW = r'->'
+  #ARROW = r'->'
+  TYPE_INT = r'TYPE_INT'
+  TYPE_REAL = r'TYPE_REAL'
+  TYPE_STRING = r'TYPE_STRING'
+  PIN_TYPE = r'OUTPUT|INPUT'
+  BOOL = r'TRUE|FALSE'
   NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
   STRING = r'\".*?\"'
   EQEQ = r'=='
+  BIGGER_OR_EQUAL = r'>='
+  SMALLER_OR_EQUAL = r'<='
+  BIGGER = r'>'
+  SMALLER = r'<'
 
   @_(r'\d+$')
   def INT(self, t):
-    print(t.value)
-    print("inteiro")
+    #print(t.value)
+    #print("inteiro")
     t.value = int(t.value)
     return t
 
   @_(r'\d+.\d+')
   def REAL(self, t):
-    print("real")
+    #print("real")
     t.value = float(t.value)
     return t
 
@@ -105,7 +123,7 @@ class BasicLexer(Lexer):
     self.lineno = t.value.count('\n')
 
   def error(self, t):
-    print("'%s' D'oh! Illegal character!" % t.value[0])
+    sys.stderr.write(f'O caractere {t.value[0]} não é aceito nesta linguagem!')
     self.index += 1
 
 
@@ -115,7 +133,6 @@ class BasicParser(Parser):
   precedence = (
     ('left', '+', '-'),
     ('left', '*', '/'),
-    ('right', 'UMINUS'),
   )
 
   def __init__(self):
@@ -129,51 +146,114 @@ class BasicParser(Parser):
   def statement(self, p):
     return ('for_loop', ('for_loop_setup', p.var_assign, p.expr), p.statement)
 
-  @_('IF condition THEN statement ELSE statement')
+  @_('IF condition THEN statement END')
   def statement(self, p):
-    return ('if_stmt', p.condition, ('branch', p.statement0, p.statement1))
+    return ('if_stmt', p.condition, ('branch', p.statement))
 
-  @_('WHILE "(" condition ")" "{" statement "}"')
+  @_('IF condition THEN statement ELSE statement END')
   def statement(self, p):
-    print(p.WHILE)
-    print(p.expr)
-    wait = input("Press Enter to continue.")
-    return ('while', p.WHILE, p.expr)
+    return ('if_stmt_else', p.condition, ('branch', p.statement0,
+                                          p.statement1))
 
-  @_('DIGITAL_READ "(" expr ")" ";"')
+  @_('DO "{" statement "}" WHILE "(" condition ")" END')
   def statement(self, p):
-    print(p.DIGITAL_READ)
-    print(p.expr)
-    wait = input("Press Enter to continue.")
+    return ('do_while', p.DO, p.statement, ('branch', p.condition))
+
+  @_('WHILE "(" condition ")" LBRACE')
+  def statement(self, p):
+    return ('open_while', p.WHILE, p.condition)
+
+  @_('WHILE "(" condition ")" "{" statement "}" END')
+  def statement(self, p):
+    return ('while', p.WHILE, ('branch', p.condition, p.statement))
+
+  @_('RBRACE END')
+  def statement(self, p):
+    return ('close_statement', p.END)
+
+  @_('DIGITAL_READ "(" expr ")"')
+  def statement(self, p):
     return ('dig_read_call', p.DIGITAL_READ, p.expr)
 
   @_('DIGITAL_WRITE "(" expr "," expr ")"')
   def statement(self, p):
-    print(p.DIGITAL_WRITE)
-    print(p.expr0)
-    print(p.expr1)
-    wait = input("Press Enter to continue.")
     return ('dig_write_call', p.DIGITAL_WRITE, p.expr0, p.expr1)
 
-  @_('FUN NAME "(" ")" ARROW statement')
+  @_('ANALOG_READ "(" expr ")"')
   def statement(self, p):
-    return ('fun_def', p.NAME, p.statement)
+    return ('analog_read_call', p.ANALOG_READ, p.expr)
 
-  @_('NAME "(" ")"')
+  @_('ANALOG_WRITE "(" expr "," expr ")"')
   def statement(self, p):
-    return ('fun_call', p.NAME)
+    return ('analog_write_call', p.ANALOG_WRITE, p.expr0, p.expr1)
+
+  @_('PIN_MODE "(" expr "," PIN_TYPE ")"')
+  def statement(self, p):
+    return ('pin_mode_call', p.PIN_MODE, ('branch', p.expr, p.PIN_TYPE))
+
+  @_('DELAY "(" expr ")"')
+  def statement(self, p):
+    return ('delay_call', p.DELAY, p.expr)
+
+  #@_('FUN NAME "(" ")" ARROW statement')
+  #def statement(self, p):
+  #  return ('fun_def', p.NAME, p.statement)
+
+  #@_('NAME "(" ")"')
+  #def statement(self, p):
+  #  return ('fun_call', p.NAME)
+
+  @_('BOOL')
+  def condition(self, p):
+    return ('condition_bool', p.BOOL)
 
   @_('expr EQEQ expr')
   def condition(self, p):
     return ('condition_eqeq', p.expr0, p.expr1)
 
+  @_('expr BIGGER_OR_EQUAL expr')
+  def condition(self, p):
+    return ('condition_bigger_equal', p.expr0, p.expr1)
+
+  @_('expr SMALLER_OR_EQUAL expr')
+  def condition(self, p):
+    return ('condition_smaller_equal', p.expr0, p.expr1)
+
+  @_('expr BIGGER expr')
+  def condition(self, p):
+    return ('condition_bigger', p.expr0, p.expr1)
+
+  @_('expr SMALLER expr')
+  def condition(self, p):
+    return ('condition_smaller', p.expr0, p.expr1)
+
   @_('var_assign')
   def statement(self, p):
     return p.var_assign
 
-  @_('NAME "=" expr')
+  @_('TYPE_INT var_assign')
+  def statement(self, p):
+    return ('typeint_var_assign', p.TYPE_INT, p.var_assign)
+
+  @_('TYPE_REAL var_assign')
+  def statement(self, p):
+    return ('type_float_var_assign', p.TYPE_REAL, p.var_assign)
+
+  @_('TYPE_STRING var_assign')
+  def statement(self, p):
+    return ('type_string_var_assign', p.TYPE_STRING, p.var_assign)
+
+  @_('NAME "=" INT')
   def var_assign(self, p):
-    return ('var_assign', p.NAME, p.expr)
+    return ('var_assign', p.NAME, p.INT)
+
+  @_('NAME "=" REAL')
+  def var_assign(self, p):
+    return ('var_assign', p.NAME, p.REAL)
+
+  #@_('NAME "=" expr')
+  #def var_assign(self, p):
+  #  return ('var_assign', p.NAME, p.expr)
 
   @_('NAME "=" STRING')
   def var_assign(self, p):
@@ -199,9 +279,13 @@ class BasicParser(Parser):
   def expr(self, p):
     return ('div', p.expr0, p.expr1)
 
-  @_('"-" expr %prec UMINUS')
+  #@_('"-" expr %prec UMINUS')
+  #def expr(self, p):
+  #  return p.expr
+
+  @_('PIN_TYPE')
   def expr(self, p):
-    return p.expr
+    return ('pin_type', p.PIN_TYPE)
 
   @_('NAME')
   def expr(self, p):
@@ -216,118 +300,77 @@ class BasicParser(Parser):
     return ('num', float(p.REAL))
 
 
-class BasicExecute:
+# Classe para capturar os erros léxicos e sintáticos do comilador
+class LogErro():
 
-  def __init__(self, tree, env):
-    self.env = env
-    result = self.walkTree(tree)
-    if result is not None and isinstance(result, int):
-      print(float("{:.2f}".format(result)))
-    if result is not None and isinstance(result, float):
-      print(float("{:.2f}".format(result)))
+  STDERR = ''
 
-    if isinstance(result, str) and result[0] == '"':
-      print(result)
+  def new_stderr(old):
 
-  def walkTree(self, node):
+    def new(*args):
+      print('Intercepted: ' + repr(args))
 
-    if isinstance(node, int):
-      return node
-    if isinstance(node, float):
-      return node
-    if isinstance(node, str):
-      return node
+    return new
 
-    if node is None:
-      return None
+  sys.stderr.write = new_stderr(sys.stderr.write)
 
-    if node[0] == 'program':
-      if node[1] == None:
-        self.walkTree(node[2])
-      else:
-        self.walkTree(node[1])
-        self.walkTree(node[2])
 
-    if node[0] == 'num':
-      return node[1]
+def identificaArquivo():
+  arquivos = []
+  for arquivo in os.listdir("."):
+    if arquivo.endswith(".txt"):
+      arquivos.append(arquivo)
+  return arquivos
 
-    if node[0] == 'str':
-      return node[1]
 
-    if node[0] == 'if_stmt':
-      result = self.walkTree(node[1])
-      if result:
-        return self.walkTree(node[2][1])
-      return self.walkTree(node[2][2])
-
-    if node[0] == 'condition_eqeq':
-      return self.walkTree(node[1]) == self.walkTree(node[2])
-
-    if node[0] == 'dig_read_call':
-      print("foi")
-      print(node[1])
-      return self.walkTree(node[1])
-
-    if node[0] == 'fun_def':
-      self.env[node[1]] = node[2]
-
-    if node[0] == 'fun_call':
-      try:
-        return self.walkTree(self.env[node[1]])
-      except LookupError:
-        print("Undefined function '%s'" % node[1])
-        return 0
-
-    if node[0] == 'add':
-      return self.walkTree(node[1]) + self.walkTree(node[2])
-    elif node[0] == 'sub':
-      return self.walkTree(node[1]) - self.walkTree(node[2])
-    elif node[0] == 'mul':
-      return self.walkTree(node[1]) * self.walkTree(node[2])
-    elif node[0] == 'div':
-      return self.walkTree(node[1]) / self.walkTree(node[2])
-
-    if node[0] == 'var_assign':
-      self.env[node[1]] = self.walkTree(node[2])
-      return node[1]
-
-    if node[0] == 'var':
-      try:
-        return self.env[node[1]]
-      except LookupError:
-        print("Undefined variable '" + node[1] + "' found!")
-        return 0
-
-    if node[0] == 'for_loop':
-      if node[1][0] == 'for_loop_setup':
-        loop_setup = self.walkTree(node[1])
-
-        loop_count = self.env[loop_setup[0]]
-        loop_limit = loop_setup[1]
-
-        for i in range(loop_count + 1, loop_limit + 1):
-          res = self.walkTree(node[2])
-          if res is not None:
-            print(res)
-          self.env[loop_setup[0]] = i
-        del self.env[loop_setup[0]]
-
-    if node[0] == 'for_loop_setup':
-      return (self.walkTree(node[1]), self.walkTree(node[2]))
-
+if sys.argv[1:]:
+  arquivos = sys.argv[1:]
+else:
+  arquivos = identificaArquivo()
+  print(arquivos)
+  wait = input("Press Enter to continue.")
 
 if __name__ == '__main__':
   lexer = BasicLexer()
   parser = BasicParser()
+  LogErro()
   env = {}
   while True:
-    try:
-      text = input('prog_for_dummies > ')
-    except EOFError:
-      break
-    if text:
-      tree = parser.parse(lexer.tokenize(text))
-      for token in lexer.tokenize(text):
-        print(token)
-      print(tree)
-      BasicExecute(tree, env)
+    opcao = input('prog_made_by_dummies ([0] read_file / [1] input_text) > ')
+    if opcao == '0':
+      #try:
+      # Verifica se existem arquivos para análise
+      # ou se foram informados na chamada do programa
+      x = 0
+      for arquivo in arquivos:
+        arq = open(arquivo, 'r')
+        print(arq)
+        wait = input("Press Enter to continue.")
+        linhas = arq.readlines()
+        print(linhas[0].strip())
+        wait = input("Press Enter to continue.")
+        for linha in linhas:
+          x += 1
+          text = linha.strip()
+          print("L{}: {}".format(x, text))
+          #text = input('prog_made_by_dummies > ')
+          #except EOFError:
+          # break
+          if text:
+            try:
+              tree = parser.parse(lexer.tokenize(text))
+            except:
+              pass
+            for token in lexer.tokenize(text):
+              print(token)
+            print(tree)
+    else:
+      try:
+        text = input('prog_made_by_dummies > ')
+      except EOFError:
+        break
+      if text:
+        tree = parser.parse(lexer.tokenize(text))
+        for token in lexer.tokenize(text):
+          print(token)
+        print(tree)
